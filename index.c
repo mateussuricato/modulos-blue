123f947785cc3f149c3bc699cc3a12751f01ab8a
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-03-16 22:14:09 -0300

    Create index.c

---
+++ index.c
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+    int main() {
+    int idade;
+
+    cin >> idade;
+
+    if (idade % 2 == 0) {
+        cout << "O numero é par";
+    } else {
+        count << "O número é ímpar";
+    }
+    
+
+    return 0;
+}
+
